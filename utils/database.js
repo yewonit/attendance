23e25ef4d04fc15cb1086411df155e53a3d34e77
isaac <icetime963@gmail.com>
2025-01-03 14:34:51 +0900

    refactor

---
+++ utils/database.js
@@ -0,0 +1,24 @@
+const { sequelize } = require("./models/models");
+
+const initDatabase = async () => {
+	try {
+		await sequelize.authenticate();
+		logger.info("데이터베이스 연결 성공");
+
+		await sequelize.query("SET FOREIGN_KEY_CHECKS = 0");
+		await sequelize.sync({ force: false });
+		await sequelize.query("SET FOREIGN_KEY_CHECKS = 1");
+
+		logger.info("데이터베이스 동기화 완료");
+	} catch (error) {
+		logger.error("데이터베이스 초기화 실패", {
+			error: error.message,
+			stack: error.stack,
+		});
+		throw error; // 상위에서 처리하도록 에러 전파
+	}
+};
+
+module.exports = {
+	initDatabase,
+};
