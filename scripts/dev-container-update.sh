7f173d645e3d4f40a241581f4292f059fe02bef1
Janghun Lee <icetime963@gmail.com>
2025-03-07 20:47:32 +0900

    prod 배포테스트 (#27)
    * feat: git secret 이메일 추가 (#21)
    
    * Fix role when user create (#24)
    
    * feat: 유저 생성 시 role id를 org id 로 찾아서 넣어주게 수정
    
    * fix: 새로운 유저 등록 시 role을 순원으로 등록
    
    * delete: node mailer 삭제
    
    * fix: deploy-dev (#25)
    
    Co-authored-by: isaac <isaac@tpirates.com>
    
    * prod 배포 파이프라인 수정 (#26)
    
    * fix: deploy-dev
    
    * feat: deploy prod
    
    ---------
    
    Co-authored-by: isaac <isaac@tpirates.com>
    
    ---------
    
    Co-authored-by: isaac <isaac@tpirates.com>

--- scripts/dev-container-update.sh
+++
@@ -1,4 +0,0 @@
-docker stop attendance-dev
-docker rm attendance-dev
-docker pull coramdeoyouth/attendance
-docker run -d -p 3001:3000 --name attendance-dev -e NODE_ENV=development coramdeoyouth/attendance
