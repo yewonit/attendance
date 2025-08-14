e32edaa4b892ba2702ed76099087d9232759e4d7
isaac <icetime963@gmail.com>
2025-08-15 02:44:19 +0900

    feat: activity template api

--- src/router/activity/activity.js
+++ src/router/activity/activity.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
+import activityService from "../../services/activity/activity";
 
 const router = Router();
 
 // TODO: organization의활동 관련 라우터 여기로 수정 필요
-// router.use("/", activityCrudRouter);
+router.get("", async (req, res, next) => {
+  res.status(200).json({
+    data: activityService.getActivityTemplate(),
+    error: null
+  })
+});
 
 export default router;
--- src/services/activity/activity.js
+++ src/services/activity/activity.js
@@ -1,8 +1,13 @@
-// import models from "../../models/models.js";
+import { activityTemplate } from "../../enums/activity_template.js";
 
 // TODO: organization의 활동 관련 서비스 구현
 const activityService = {
-
+	getActivityTemplate: () => {
+		const result = []
+		for (let template of Object.values(activityTemplate)) {
+			result.push(template)
+		}
+	}
 };
 
 // 모듈을 내보내어 라우트 등 다른 파트에서 사용할 수 있도록 합니다.
