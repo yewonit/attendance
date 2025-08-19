bb56232837c15498328024fb49833ef6dee5caee
isaac <isaac@tpirates.com>
2025-08-19 11:55:24 +0900

    fix: activity router

--- src/router/activity/activity.js
+++ src/router/activity/activity.js
@@ -4,72 +4,91 @@ import activityService from "../../services/activity/activity";
 const router = Router();
 
 router.get("", async (req, res, next) => {
-  res.status(200).json({
-    data: activityService.getActivityTemplate(),
-    error: null
-  })
+	const { organizationId } = req.query;
+	try {
+		const data = await activityService.getAllOrganizationActivities(
+			organizationId
+		);
+		res.status(200).json({
+			data: data,
+			error: null,
+		});
+	} catch (error) {
+		next(error);
+	}
 });
 
-router.post("/activities", async (req, res, next) => {
-  const { organizationId, activityTemplateId } = req.query;
-  const data = req.body;
-  try {
-    await activityService.recordActivityAndAttendance(organizationId, activityTemplateId, data)
-    res.status(201).json({
-      data: "success",
-      error: null
-    })
-  } catch (error) {
-    next(error)
-  }
-})
+router.post("", async (req, res, next) => {
+	const { organizationId, activityTemplateId } = req.query;
+	const data = req.body;
+	try {
+		await activityService.recordActivityAndAttendance(
+			organizationId,
+			activityTemplateId,
+			data
+		);
+		res.status(201).json({
+			data: "success",
+			error: null,
+		});
+	} catch (error) {
+		next(error);
+	}
+});
 
-router.get("/activities/:id", async (req, res, next) => {
-  const activityId = req.path.id
+router.get("/templates", async (req, res, next) => {
+	res.status(200).json({
+		data: activityService.getActivityTemplate(),
+		error: null,
+	});
+});
 
-  try {
-    const data = await activityService.getActivityDetails(activityId)
-    const response = {
-      data: data,
-      error: null
-    }
+router.get("/:id", async (req, res, next) => {
+	const activityId = req.path.id;
 
-    res.status(200).json({
-      data: response,
-      error: null
-    })
-  } catch (error) {
-    next(error)
-  }
-})
+	try {
+		const data = await activityService.getActivityDetails(activityId);
+		const response = {
+			data: data,
+			error: null,
+		};
 
-router.put("/activities/:id", async (req, res, next) => {
-  const activityId = req.path.id
-  const data = req.body
+		res.status(200).json({
+			data: response,
+			error: null,
+		});
+	} catch (error) {
+		next(error);
+	}
+});
 
-  try {
-    await activityService.updateActivityAndAttendance(activityId, data)
-    res.status(200).json({
-      data: "success",
-      error: null
-    })
-  } catch (error) {
-    next(error)
-  }
-})
+router.put("/:id", async (req, res, next) => {
+	const activityId = req.path.id;
+	const data = req.body;
 
-router.delete("/activities/:id", async (req, res, next) => {
-  const activityId = req.path.id
+	try {
+		await activityService.updateActivityAndAttendance(activityId, data);
+		res.status(200).json({
+			data: "success",
+			error: null,
+		});
+	} catch (error) {
+		next(error);
+	}
+});
 
-  try {
-    await activityService.deleteActivityAndAttendance(activityId)
-    res.status(200).json({
-      data: "success",
-      error: null
-    })
-  } catch (error) {
-    next(error)
-  }
-})
+router.delete("/:id", async (req, res, next) => {
+	const activityId = req.path.id;
+
+	try {
+		await activityService.deleteActivityAndAttendance(activityId);
+		res.status(200).json({
+			data: "success",
+			error: null,
+		});
+	} catch (error) {
+		next(error);
+	}
+});
 
 export default router;
--- src/services/activity/activity.js
+++ src/services/activity/activity.js
@@ -4,12 +4,66 @@ import { DataCreationError, ValidationError } from "../../utils/errors.js";
 // TODO: organization의 활동 관련 서비스 구현
 const activityService = {
 	getActivityTemplate: () => {
-		const result = []
+		const result = [];
 		for (let template of Object.values(activityTemplate)) {
-			result.push(template)
+			result.push(template);
 		}
 	},
-	// 활동 상세 정보 조회
+	getAllOrganizationActivities: async (organizationId) => {
+		const activities = await models.Activity.findAll({
+			where: { organization_id: organizationId },
+			include: [
+				{
+					model: models.Attendance,
+					include: [
+						{
+							model: models.User,
+							where: { id: models.Attendance.user_id },
+							attributes: ["id", "name", "email"],
+						},
+					],
+				},
+				{
+					model: models.ActivityImage,
+					where: { activity_id: models.Activity.id },
+				},
+			],
+		});
+
+		if (!activities || activities.length === 0) {
+			return null;
+		}
+
+		const result = activities.map((activity) => {
+			return {
+				activity: {
+					id: activity.id,
+					activityCategory: activity.activity_category,
+					location: activity.location,
+					startDateTime: activity.start_time,
+					endDateTime: activity.end_time,
+					notes: activity.description,
+					name: activity.name,
+					description: activity.description,
+					attendances: activity.Attendances.map((attendance) => ({
+						id: attendance.id,
+						userId: attendance.User.id,
+						userName: attendance.User.name,
+						userEmail: attendance.User.email,
+						status: attendance.attendance_status,
+						note: attendance.description,
+					})),
+					images: activity.ActivityImages.map((image) => ({
+						id: image.id,
+						name: image.name,
+						path: image.path,
+					})),
+				},
+			};
+		});
+
+		return result;
+	},
 	getActivityDetails: async (activityId) => {
 		const activity = await models.Activity.findOne({
 			where: { id: activityId },
@@ -32,7 +86,7 @@ const activityService = {
 		});
 
 		if (!activity) {
-			return null
+			return null;
 		}
 
 		return {
@@ -61,36 +115,45 @@ const activityService = {
 			},
 		};
 	},
-	recordActivityAndAttendance: async (organizationId, activityTemplateId, data) => {
+	recordActivityAndAttendance: async (
+		organizationId,
+		activityTemplateId,
+		data
+	) => {
 		const { activityData, attendances, imageInfo } = data;
 
 		if (!activityTemplateId || !activityData || !attendances) {
-			throw new ValidationError(`필수 데이터가 누락되었습니다. activityId: ${activityId}, activityData: ${activityData}, attendances: ${attendances}`)
+			throw new ValidationError(
+				`필수 데이터가 누락되었습니다. activityId: ${activityId}, activityData: ${activityData}, attendances: ${attendances}`
+			);
 		}
 
 		if (!activityData.startDateTime || !activityData.endDateTime) {
-			throw new ValidationError("시작 시간과 종료 시간은 필수입니다.")
+			throw new ValidationError("시작 시간과 종료 시간은 필수입니다.");
 		}
 
 		const organization = await models.Organization.findByPk(organizationId);
 		if (!organization) {
-			throw new ValidationError("조직을 찾을 수 없습니다.")
+			throw new ValidationError("조직을 찾을 수 없습니다.");
 		}
 
 		const template = Object.values(activityTemplate).find(
 			(at) => at.id === activityId
 		);
 		if (!template) {
-			throw new ValidationError("해당 활동 템플릿을 찾을 수 없습니다.")
+			throw new ValidationError("해당 활동 템플릿을 찾을 수 없습니다.");
 		}
 
-		if (!(imageInfo &&
-			typeof imageInfo === "object" &&
-			imageInfo.url &&
-			imageInfo.fileName &&
-			imageInfo.fileType)
+		if (
+			!(
+				imageInfo &&
+				typeof imageInfo === "object" &&
+				imageInfo.url &&
+				imageInfo.fileName &&
+				imageInfo.fileType
+			)
 		) {
-			throw new ValidationError("올바르지 않은 imageInfo : ", imageInfo)
+			throw new ValidationError("올바르지 않은 imageInfo : ", imageInfo);
 		}
 
 		try {
@@ -117,10 +180,9 @@ const activityService = {
 					attendance_status: attendance.status,
 					description: attendance.note,
 				});
-			})
-
+			});
 		} catch (error) {
-			throw new DataCreationError("활동 정보 저장 중 에러 발생 : ", error)
+			throw new DataCreationError("활동 정보 저장 중 에러 발생 : ", error);
 		}
 	},
 
@@ -128,16 +190,14 @@ const activityService = {
 		const { activityData, attendances, imageInfo } = data;
 
 		if (!activityId || !activityData || !attendances) {
-			throw new ValidationError("필수 데이터가 누락되었습니다.")
+			throw new ValidationError("필수 데이터가 누락되었습니다.");
 		}
 
 		const activity = await models.Activity.findOne({
 			where: { id: activityId },
 		});
 		if (!activity) {
-			return res
-				.status(404)
-				.json({ message: "활동을 찾을 수 없습니다." });
+			return res.status(404).json({ message: "활동을 찾을 수 없습니다." });
 		}
 
 		await activity.update({
@@ -147,11 +207,7 @@ const activityService = {
 			description: activityData.notes,
 		});
 
-		if (
-			imageInfo &&
-			imageInfo.url &&
-			imageInfo.fileName
-		) {
+		if (imageInfo && imageInfo.url && imageInfo.fileName) {
 			const existingFile = await models.ActivityImage.findOne({
 				where: { activity_id: activityId },
 			});
@@ -201,15 +257,14 @@ const activityService = {
 
 			const attendances = await models.Attendance.findAll({
 				where: {
-					activity_id: activityId
-				}
-			})
-			attendances.forEach((attendance) => {
+					activity_id: activityId,
+				},
+			});
+			attendances.forEach(async (attendance) => {
 				await attendance.destroy();
-			})
+			});
 		});
-	}
-},
+	},
 };
 
 // 모듈을 내보내어 라우트 등 다른 파트에서 사용할 수 있도록 합니다.
